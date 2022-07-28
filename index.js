fe4b69d429ff11df2e9482be1895c815a7245f7f
Miranda Wilson <miranda@snowplowanalytics.com>
2022-07-28 15:59:31 +0100

    Log branch names

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ const github = require("@actions/github");
 console.log("Hello from index.js");
 
 const context = github.context;
-console.log(`This is the github context: \n ${context}`);
 console.log(context.actor);
 console.log(context.ref);
 
 try {
+  const branchName = context.ref.replace("refs/heads/", "");
+  const branchType = branchName.split("/")[0];
+
+  console.log(`branch name is: ${branchName}`);
+  switch (branchType) {
+    case "release":
+      console.log("it's a release branch!");
+      break;
+    case "issue":
+      console.log("it's an issue branch!");
+      break;
+    case ("main", "master"):
+      console.log("we're on main street");
+      break;
+    default:
+      console.log("what's this random branch?");
+  }
+
   const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
