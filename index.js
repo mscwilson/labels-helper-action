216d3a72257c170ce2b05f7d6f69d4a715cb9a4f
Miranda Wilson <miranda@snowplowanalytics.com>
2022-07-29 11:24:27 +0100

    Add gitignore

--- index.js
+++ index.js
@@ -3,36 +3,51 @@ const github = require("@actions/github");
 
 console.log("Hello from index.js");
 
-const context = github.context;
-console.log(context.actor);
-console.log(context.ref);
+async function run() {
+  const accessToken = core.getInput("ACCESS_TOKEN");
+  const octokit = github.getOctokit(accessToken);
 
-try {
+  const context = github.context;
+
   const branchName = context.ref.replace("refs/heads/", "");
   const branchType = branchName.split("/")[0];
 
-  console.log(`branch name is: ${branchName}`);
-  switch (branchType) {
-    case "release":
-      console.log("it's a release branch!");
-      break;
-    case "issue":
-      console.log("it's an issue branch!");
-      break;
-    case ("main", "master"):
-      console.log("we're on main street");
-      break;
-    default:
-      console.log("what's this random branch?");
-  }
+  const { data: commits } = await octokit.rest.repos.listCommits({
+    owner: "mscwilson",
+    repo: "try-out-actions-here",
+    sha: branchName,
+  });
 
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+  console.log(commits[0]);
 }
+
+run();
+
+// try {
+
+//   console.log(`branch name is: ${branchName}`);
+//   switch (branchType) {
+//     case "release":
+//       console.log("it's a release branch!");
+//       break;
+//     case "issue":
+//       console.log("it's an issue branch!");
+//       break;
+//     case "main":
+//     case "master":
+//       console.log("we're on main street");
+//       break;
+//     default:
+//       console.log("what's this random branch?");
+//   }
+
+//   const nameToGreet = core.getInput("who-to-greet");
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = new Date().toTimeString();
+//   core.setOutput("time", time);
+
+//   const payload = JSON.stringify(github.context.payload, undefined, 2);
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
