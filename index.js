6a4a6c1f5209d4988d2b54315cbd35a1a804ca02
Miranda Wilson <miranda@snowplowanalytics.com>
2022-07-29 13:26:17 +0100

    Add completed label

--- index.js
+++ index.js
@@ -1,24 +1,69 @@
+require("dotenv").config();
 const core = require("@actions/core");
 const github = require("@actions/github");
 
 console.log("Hello from index.js");
 
+const commitRegex =
+  /^([\w\s.,'"-:`@]+) \((?:close|closes|fixes|fix) \#(\d+)\)$/;
+
 async function run() {
   const accessToken = core.getInput("ACCESS_TOKEN");
   const octokit = github.getOctokit(accessToken);
+  // const octokit = github.getOctokit(process.env.PAT);
+  // const owner = "mscwilson";
+  // const repo = "try-out-actions-here";
+  // const branchName = "release/0.2.0";
+  // const issueNumber = 5;
 
   const context = github.context;
+  const owner = context.repository.owner.name;
+  const repo = context.repository.name;
 
   const branchName = context.ref.replace("refs/heads/", "");
   const branchType = branchName.split("/")[0];
 
   const { data: commits } = await octokit.rest.repos.listCommits({
-    owner: "mscwilson",
-    repo: "try-out-actions-here",
+    owner: owner,
+    repo: repo,
     sha: branchName,
   });
 
-  console.log(commits[0]);
+  const commitMessage = commits[0].commit.message;
+  const issueNumber = commitMessage.match(commitRegex)[2];
+
+  try {
+    await octokit.rest.issues.addLabels({
+      owner: owner,
+      repo: repo,
+      issue_number: issueNumber,
+      labels: ["status:completed"],
+    });
+  } catch (error) {
+    console.log(`Couldn't find issue #${issueNumber}`);
+  }
+
+  try {
+    await octokit.rest.issues.removeLabel({
+      owner: owner,
+      repo: repo,
+      issue_number: issueNumber,
+      name: "status:in_progress",
+    });
+  } catch (RequestError) {
+    // that's fine
+  }
+
+  try {
+    await octokit.rest.issues.removeLabel({
+      owner: owner,
+      repo: repo,
+      issue_number: issueNumber,
+      name: "status:has_pr",
+    });
+  } catch (RequestError) {
+    // that's fine
+  }
 }
 
 run();
